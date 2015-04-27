c9b11f0d95ded0282b89c920ada9edcbd13ebf3a
Sky <lxyxztk777@163.com>
2015-04-27 19:47:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aaa/bluetooth/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.aaa.bluetooth;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+
+}
